87190a44aa7e1f2b11d84e322b96d148082f182e
Horqu <pawel.szewczuk.h@gmail.com>
2022-03-04 11:47:50 +0100

    Doxygen documentation added to l1

--- Laboratories/l1/include/MyList.h
+++ Laboratories/l1/include/MyList.h
@@ -3,22 +3,33 @@
 #include <iostream>
 #include <utility>
 
-
+/**
+ * @brief Class representing node(unfinished, only to make MyList class work) 
+ * 
+ */
 class MyNode{
 
-public:
+private:
 
     int _value;
 
     MyNode() : _value(0){}
+
+public:
 
     int val() const{
 
         return _value;
     }
 
+    friend class MyList;
+
 };
 
+/**
+ * @brief Class representing list
+ * 
+ */
 class MyList{
 
 private:
@@ -31,11 +42,20 @@ private:
 
 public:
 
+    /**
+     * @brief Construct a new My List object
+     * 
+     */
     MyList() : _size(0), _begin(nullptr), _end(nullptr){
 
         std::cout << "KONST: Domyslny" << std::endl;
     }
 
+    /**
+     * @brief Construct a new My List object
+     * 
+     * @param n - size of created list
+     */
     MyList(int n) : _size(n){
 
         std::cout << "KONST: Domyslny" << std::endl << "KONST: Jednoargumentowy" << std::endl;
@@ -52,6 +72,11 @@ public:
 
     }
 
+    /**
+     * @brief Construct a new My List object by using initializer list
+     * 
+     * @param n - initializer list
+     */
     MyList(std::initializer_list<int> n){
 
         std::cout << "KONST: std::initializer_list" << std::endl;
@@ -70,17 +95,32 @@ public:
 
     }
 
-
+    /**
+     * @brief Construct a new My List object
+     * 
+     * @param l - source object used to copy
+     */
     MyList(const MyList &l) : _begin(l._begin), _end(l._end), _size(l._size){
         
         std::cout << "KONST: Domyslny" << std::endl << "KONST: kopiujacy" << std::endl;
     }
 
+    /**
+     * @brief Construct a new My List object
+     * 
+     * @param l - source object which is being moved
+     */
     MyList(MyList &&l) : _size(std::exchange(l._size, 0)), _begin(std::move(l._begin)), _end(std::move(l._end)){
 
         std::cout << "KONST: przenoszacy" << std::endl;
     }
 
+    /**
+     * @brief Construct a new My List object
+     * 
+     * @param func - function from main.cpp which is returning integer number
+     * @param n - size of a list
+     */
     MyList(int (&func)(MyNode *), int n) : _size(n){
 
         std::cout << "KONST: funkcja generujaca" << std::endl;
@@ -106,6 +146,10 @@ public:
 
     }
 
+    /**
+     * @brief Destroy the My List object
+     * 
+     */
     ~MyList(){
 
         std::cout << "DESTRUKTOR (rozmiar = " << _size << ")" << std::endl;
@@ -113,6 +157,10 @@ public:
         this->clear();
     }
 
+    /**
+     * @brief Frees allocated memory for object without deleting it
+     * 
+     */
     void clear(){
 
         if(_size == 0) return;
@@ -127,6 +175,11 @@ public:
 
     }
 
+    /**
+     * @brief Add a new integer to the end of list
+     * 
+     * @param n - new added value
+     */
     void add(int n){
 
         _size++;
@@ -146,6 +199,12 @@ public:
 
     }
 
+    /**
+     * @brief Check if list is empty
+     * 
+     * @return true - when empty
+     * @return false - when there is at least one number in the list
+     */
     bool empty() const{
 
         if(_size > 0) return false;
@@ -153,11 +212,20 @@ public:
         return true;
     }
 
+    /**
+     * @brief Returns size of the list 
+     * 
+     * @return int - size
+     */
     int size() const{
 
         return _size;
     }
 
+    /**
+     * @brief Print whole list
+     * 
+     */
     void print() const{
 
         std::cout << "[";
